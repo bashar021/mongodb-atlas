5c9ff113021f776a3deabd18cf70fad08a410cbc
bashar021 <alambashar021@gmail.com>
2023-02-14 21:03:42 +0530

    version 1.0

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json())
 
-    const DB =process.env.DB_URL
+    const DB = process.env.DB_URL
     mongoose.connect(DB,{ useNewUrlParser: true, useUnifiedTopology: true  })
     .then(() => console.log('Connected!'))
     .catch(err=> console.log(err));
